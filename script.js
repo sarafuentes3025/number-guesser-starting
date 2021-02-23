532a4bbafe9a68c98940a15fb7c4330b289896e2
Sara <sara.fuentes3025@gmail.com>
2021-02-23 16:40:15 -0600

    fixed functions, add alert, and advanceround function

--- script.js
+++ script.js
@@ -2,39 +2,30 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
-
 // generateTarget Function
-const generateTarget= random => {
-  return Math.floor(Math.random()*10);
-};
+const generateTarget= () => Math.floor(Math.random()*10);
+
+const getAbsoluteDistance = (distance1, distance2) => Math.abs(distance1 -distance2);
 
 // compareGuesses Function
-const compareGuesses = (human, computer) => {
-  const random = generateTarget();
-
-  let humanClosest = random - human;
-  let computerClosest = random - computer;
-  humanClosest = Math.abs(humanClosest);
-  computerClosest = Math.abs(computerClosest);
-
-  if(computerClosest < humanClosest){
-    return false;
-  }else{
+const compareGuesses = (human, computer, target) => {
+  if(human > 9 || human < 0 ){
+    window.alert('Please choose number between 0 and 9.');
+  }else if(Math.abs(human - target) <= Math.abs(computer - target) || Math.abs(human - target) === Math.abs(computer - target)){
     return true;
-  }
+  }else{
+    return false;
+  } 
 };
 
-// const human = compareGuesses(9,2);
-
+// updateScore Function
 const updateScore = guesser => {
   if(guesser === 'human'){
-    return humanScore+=1;
-
-  }else if(guesser === 'computer'){
-    return computerScore+=1;
+    return humanScore++;
+  }else{
+    return computerScore++;
   }  
-}
-// console.log(updateScore('human'));
+};
 
-
+// advanceround Function
+const advanceRound = () => currentRoundNumber ++;
